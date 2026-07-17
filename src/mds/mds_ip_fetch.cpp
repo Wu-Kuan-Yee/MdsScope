40783ec62d8e2011aaeba226bee1bb3419553d42
gywu <wu_gy@mail.ustc.edu.cn>
2026-07-18 01:44:42 +0800

    Conditionally restore NoProxy for non-mobile platforms to bypass system proxies

--- src/mds/mds_ip_fetch.cpp
+++ src/mds/mds_ip_fetch.cpp
@@ -48,6 +48,9 @@ QVector<SignalFetchResult> MdsIpClient::fetchGroupResults(const QVector<NativeRe
             if (!canReuseSocket) {
                 resetConnection(cached);
                 cached->socket = std::make_unique<QTcpSocket>();
+#if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
+                cached->socket->setProxy(QNetworkProxy::NoProxy);
+#endif
                 stageTimer.restart();
                 cached->socket->connectToHost(serverHost(firstSig.serverIp), serverPort(firstSig.serverIp));
                 QElapsedTimer connectTimer;
